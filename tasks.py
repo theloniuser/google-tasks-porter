7a3280f215080cd7deb76a000bba14fb2b28c90f
dwightguth <dwightguth@localhost>
2011-07-19 14:45:01 -0500

    Initial Commit

---
+++ tasks.py
@@ -0,0 +1,400 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2011 Google Inc.  All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Main web application handler for Google Tasks Takeout."""
+
+__author__ = "dwightguth@google.com (Dwight Guth)"
+
+import os
+import pickle
+
+from oauth2client import appengine
+from oauth2client import client
+
+from google.appengine.api import mail
+from google.appengine.api import memcache
+from google.appengine.api import taskqueue
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import util
+
+
+import model
+import settings
+
+
+def _RedirectForOAuth(self, user):
+  """Redirects the webapp response to authenticate the user with OAuth2."""
+  flow = client.OAuth2WebServerFlow(
+      client_id=settings.CLIENT_ID + ".apps.googleusercontent.com",
+      client_secret=settings.CLIENT_SECRET,
+      scope="https://www.googleapis.com/auth/tasks",
+      user_agent="task-takeout/1.0",
+      xoauth_displayname="Google Tasks Takeout",
+      state=self.request.path_qs)
+
+  callback = self.request.relative_url("/oauth2callback")
+  authorize_url = flow.step1_get_authorize_url(callback)
+  memcache.set(user.user_id(), pickle.dumps(flow))
+  self.redirect(authorize_url)
+
+
+def _GetCredentials():
+  user = users.get_current_user()
+  credentials = appengine.StorageByKeyName(
+      model.Credentials, user.user_id(), "credentials").get()
+  return user, credentials
+
+
+class MainHandler(webapp.RequestHandler):
+  """Handler for /."""
+
+  def get(self):
+    """Handles GET requests for /."""
+    credentials = _GetCredentials()[1]
+    path = os.path.join(os.path.dirname(__file__), "index.html")
+    if not credentials or credentials.invalid:
+      template_values = {"is_authorized": False}
+      self.response.out.write(template.render(path, template_values))
+    else:
+      template_values = {"is_authorized": True}
+      self.response.out.write(template.render(path, template_values))
+
+
+class AuthRedirectHandler(webapp.RequestHandler):
+  """Handler for /auth."""
+
+  def get(self):
+    """Handles GET requests for /auth."""
+    user, credentials = _GetCredentials()
+
+    if not credentials or credentials.invalid:
+      _RedirectForOAuth(self, user)
+    else:
+      self.redirect("/")
+
+
+class ListHandler(webapp.RequestHandler):
+  """Handler for /snapshots."""
+
+  def get(self):
+    """Handles GET requests for /snapshots."""
+    user, credentials = _GetCredentials()
+
+    if not credentials or credentials.invalid:
+      _RedirectForOAuth(self, user)
+    else:
+      path = os.path.join(os.path.dirname(__file__), "snapshots.html")
+      snapshots = model.Snapshot.gql("WHERE user = :user and type = 'export'",
+                                     user=user)
+
+      counts = []
+      refresh = False
+
+      for snapshot in snapshots:
+        tasklists = model.TaskList.gql("WHERE ANCESTOR IS :id",
+                                       id=snapshot.key())
+        task_count = 0
+        tasklist_count = 0
+        if snapshot.status == "building":
+          refresh = True
+        for tasklist in tasklists:
+          task_count += tasklist.tasks.count()
+          tasklist_count += 1
+
+        counts.append((snapshot, task_count, tasklist_count))
+
+      template_values = {"snapshots": counts,
+                         "error": self.request.get("error"),
+                         "refresh": refresh}
+      self.response.out.write(template.render(path, template_values))
+
+
+class SnapshotHandler(webapp.RequestHandler):
+  """Handler for /snapshot."""
+
+  def get(self):
+    """Handles GET requests for /snapshot."""
+    user, credentials = _GetCredentials()
+
+    if not credentials or credentials.invalid:
+      _RedirectForOAuth(self, user)
+    else:
+      # need to create snapshot outside of task queue because there is no easy
+      # way to pass user identity to a task other than through a datastore
+      # entity.
+      snapshot = model.Snapshot()
+      snapshot.type = "export"
+      snapshot.user = users.get_current_user()
+      snapshot.status = "building"
+      snapshot.put()
+
+      taskqueue.add(url="/worker/snapshot",
+                    params={"id": snapshot.key().id()})
+      self.redirect("/snapshots")
+
+
+class DeleteHandler(webapp.RequestHandler):
+  """Handler for /delete."""
+
+  def get(self):
+    """Handles GET requests for /delete."""
+    user, credentials = _GetCredentials()
+
+    if not credentials or credentials.invalid:
+      self.redirect("/snapshots")
+    else:
+      snapshot = model.Snapshot.gql("WHERE user = :user "
+                                    "AND __key__ = KEY('Snapshot', :key)",
+                                    user=user,
+                                    key=int(self.request.get("id"))).get()
+
+      if snapshot.status == "building":
+        # can't delete until snapshot is done
+        self.redirect("/snapshots?error=DELETE_BUILDING")
+        return
+
+      task_entities = model.Task.gql("WHERE ANCESTOR IS :id",
+                                     id=snapshot.key())
+
+      for ent in task_entities:
+        ent.delete()
+
+      tasklist_entities = model.TaskList.gql("WHERE ANCESTOR IS :id",
+                                             id=snapshot.key())
+
+      for ent in tasklist_entities:
+        ent.delete()
+
+      snapshot.delete()
+
+      if not self.request.get("import"):
+        self.redirect("/snapshots")
+      else:
+        self.redirect("/import")
+
+
+class DownloadHandler(webapp.RequestHandler):
+  """Handler for /download."""
+
+  def get(self):
+    """Handles GET requests for /download.
+
+    This handler takes the following query parameters:
+      id: the internal id serving as key for the snapshot to download.
+      format: either "ics", "csv", or "html" depending on what format is selected
+      to download.
+    """
+    user, credentials = _GetCredentials()
+
+    if not credentials or credentials.invalid:
+      self.redirect("/snapshots")
+    else:
+      snapshot = model.Snapshot.gql("WHERE user = :user "
+                                    "AND __key__ = KEY('Snapshot', :key)",
+                                    user=user,
+                                    key=int(self.request.get("id"))).get()
+      tasklist_entities = model.TaskList.gql("WHERE ANCESTOR IS :id",
+                                             id=snapshot.key())
+      template_values = {"tasklists": list(tasklist_entities),
+                         "now": snapshot.timestamp}
+
+      if self.request.get("format") == "ics":
+        self.WriteIcsTemplate(template_values)
+      elif self.request.get("format") == "csv":
+        self.WriteCsvTemplate(template_values)
+      elif self.request.get("format") == "html":
+        self.WriteHtmlTemplate(template_values)
+
+  def WriteIcsTemplate(self, template_values):
+    self.response.headers["Content-Type"] = "text/calendar"
+    self.response.headers.add_header(
+        "Content-Disposition", "attachment; filename=tasks_%s.ics" %
+        template_values["now"].strftime("%m-%d-%Y"))
+
+    path = os.path.join(os.path.dirname(__file__), "todo.ics")
+    self.response.out.write(template.render(path, template_values))
+
+  def WriteCsvTemplate(self, template_values):
+    self.response.headers["Content-Type"] = "text/csv"
+    self.response.headers.add_header(
+        "Content-Disposition", "attachment; filename=tasks_%s.csv" %
+        template_values["now"].strftime("%m-%d-%Y"))
+
+    path = os.path.join(os.path.dirname(__file__), "todo.csv")
+    self.response.out.write(template.render(path, template_values))
+
+  def WriteHtmlTemplate(self, template_values):
+    path = os.path.join(os.path.dirname(__file__), "todo.html")
+    self.response.out.write(template.render(path, template_values))
+
+
+class ImportHandler(webapp.RequestHandler):
+  """Handler for /import."""
+
+  def get(self):
+    """Handles GET requests for /import."""
+    user, credentials = _GetCredentials()
+
+    if not credentials or credentials.invalid:
+      _RedirectForOAuth(self, user)
+    else:
+      path = os.path.join(os.path.dirname(__file__), "import.html")
+      snapshots = model.Snapshot.gql("WHERE user = :user "
+                                     "and type = 'import'",
+                                     user=user)
+
+      titles = []
+      refresh = False
+      for snapshot in snapshots:
+        if snapshot.status == "completed":
+          title = model.TaskList.gql("WHERE ANCESTOR IS :id",
+                                     id=snapshot.key()).get().title
+        else:
+          title = ""
+          if snapshot.status == "building":
+            refresh = True
+        titles.append((snapshot, title))
+
+      template_values = {"snapshots": titles,
+                         "error": self.request.get("error"),
+                         "refresh": refresh}
+      self.response.out.write(template.render(path, template_values))
+
+  def post(self):
+    """Handles POST requests for /import.
+
+    This handler takes the following query parameters:
+      name: The name of the tasklist to create and put the imported tasks into.
+      format: either "ics" or "csv" depending on whta format to import from.
+
+    The body of the POST request requires the following parameters:
+      file: a file reference containing either the ics or csv file to import.
+    """
+    if (not self.request.get("file") or
+        not self.request.get("name") or
+        not self.request.get("format")):
+      self.redirect("/import?error=REQUIRED_FIELD")
+      return
+    snapshot = model.Snapshot()
+    snapshot.type = "import"
+    snapshot.user = users.get_current_user()
+    snapshot.status = "building"
+    snapshot.put()
+
+    taskqueue.add(url="/worker/import",
+                  params={"file": self.request.get("file"),
+                          "name": self.request.get("name"),
+                          "format": self.request.get("format"),
+                          "id": snapshot.key().id()})
+    self.redirect("/import")
+
+
+class SendMailHandler(webapp.RequestHandler):
+  """Handler for /sendmail."""
+
+  def get(self):
+    """Handles GET requests for /sendmail."""
+    user, credentials = _GetCredentials()
+    if not credentials or credentials.invalid:
+      _RedirectForOAuth(self, user)
+    else:
+      path = os.path.join(os.path.dirname(__file__), "sendmail.html")
+      template_values = {"id": self.request.get("id"),
+                         "error": self.request.get("error")}
+
+      self.response.out.write(template.render(path, template_values))
+
+  def post(self):
+    """Handles POST requests for /sendmail.
+
+    This handler takes the following query parameters:
+      id: the internal id serving as key for the snapshot to mail.
+      email: the Remember The Milk import email address to send to.
+      subject: the name of the task list to create.
+    """
+    user, credentials = _GetCredentials()
+    if not credentials or credentials.invalid:
+      _RedirectForOAuth(self, user)
+    else:
+      if not self.request.get("id"):
+        self.redirect("/snapshots?error=NO_ID")
+        return
+      if (not self.request.get("email") or
+          not self.request.get("subject")):
+        self.redirect("/sendmail?id=%s&error=REQUIRED_FIELD" %
+                      self.request.get("id"))
+        return
+
+      snapshot = model.Snapshot.gql("WHERE user = :user "
+                                    "AND __key__ = KEY('Snapshot', :key)",
+                                    user=user,
+                                    key=int(self.request.get("id"))).get()
+      tasklist_entities = model.TaskList.gql("WHERE ANCESTOR IS :id",
+                                             id=snapshot.key())
+
+      template_values = {"tasklists": list(tasklist_entities),
+                         "now": snapshot.timestamp}
+
+      email_body = self.GenerateEmailBody(template_values)
+
+      mail.send_mail(sender="noreply@google.com",
+                     to=self.request.get("email"),
+                     subject=self.request.get("subject"),
+                     body=email_body)
+
+      self.redirect("/snapshots")
+
+  def GenerateEmailBody(self, template_values):
+    path = os.path.join(os.path.dirname(__file__), "todo.txt")
+    return template.render(path, template_values)
+
+
+class OAuthHandler(webapp.RequestHandler):
+  """Handler for /oauth2callback."""
+
+  def get(self):
+    """Handles GET requests for /oauth2callback."""
+    user = users.get_current_user()
+    flow = pickle.loads(memcache.get(user.user_id()))
+    if flow:
+      credentials = flow.step2_exchange(self.request.params)
+      appengine.StorageByKeyName(
+          model.Credentials, user.user_id(), "credentials").put(credentials)
+      self.redirect(self.request.get("state"))
+
+
+def main():
+  template.register_template_library("common.customdjango")
+
+  application = webapp.WSGIApplication(
+      [
+          ("/", MainHandler),
+          ("/auth", AuthRedirectHandler),
+          ("/delete", DeleteHandler),
+          ("/download", DownloadHandler),
+          ("/import", ImportHandler),
+          ("/oauth2callback", OAuthHandler),
+          ("/sendmail", SendMailHandler),
+          ("/snapshot", SnapshotHandler),
+          ("/snapshots", ListHandler)
+      ])
+  util.run_wsgi_app(application)
+
+
+if __name__ == "__main__":
+  main()
---
+++ worker.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python2.5
+#
+# Copyright 2011 Google Inc.  All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Request Handler for Task Queue Tasks."""
+
+__author__ = "dwightguth@google.com (Dwight Guth)"
+
+import logging
+
+from apiclient import discovery
+from oauth2client import appengine
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+
+import httplib2
+
+from common import apiparse
+from common import apiupload
+import csvparse
+import icalparse
+import model
+
+
+class SnapshotWorker(webapp.RequestHandler):
+  """Handler for /worker/snapshot."""
+
+  def post(self):
+    """Handles POST requests for /worker/snapshot."""
+    snapshot = model.Snapshot.gql("WHERE __key__ = KEY('Snapshot', :key)",
+                                  key=int(self.request.get("id"))).get()
+    user = snapshot.user
+    credentials = appengine.StorageByKeyName(
+        model.Credentials, user.user_id(), "credentials").get()
+
+    if credentials is None or credentials.invalid == True:
+      snapshot.status = "error"
+      snapshot.errorMessage = "Must be logged in to create snapshot."
+      snapshot.put()
+    else:
+      try:
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+        service = discovery.build("tasks", "v1", http)
+        tasklists = service.tasklists()
+        tasklists_list = tasklists.list().execute()
+
+        parser = apiparse.Parser(model.TaskList, None, snapshot, tasklists.list,
+                                 model)
+        tasklist_entities = parser.ParseAndStore(tasklists_list)
+
+        for tasklist in tasklist_entities:
+          tasks = service.tasks()
+          tasks_list = tasks.list(tasklist=tasklist.id,
+                                  showHidden=True).execute()
+          parser = apiparse.Parser(model.Task,
+                                   tasklist,
+                                   snapshot,
+                                   tasks.list,
+                                   model,
+                                   tasklist=tasklist.id,
+                                   showHidden=True)
+          parser.ParseAndStore(tasks_list)
+        snapshot.status = "completed"
+        snapshot.put()
+      except Exception, e:
+        snapshot.status = "error"
+        snapshot.errorMessage = "Snapshot creation process failed unexpectedly."
+        logging.error(e)
+        snapshot.put()
+
+
+class ImportWorker(webapp.RequestHandler):
+  """Handler for /worker/import."""
+
+  def post(self):
+    """Handles POST requests for /worker/snapshot."""
+    snapshot = model.Snapshot.gql("WHERE __key__ = KEY('Snapshot', :key)",
+                                  key=int(self.request.get("id"))).get()
+    user = snapshot.user
+    credentials = appengine.StorageByKeyName(
+        model.Credentials, user.user_id(), "credentials").get()
+
+    if credentials is None or credentials.invalid == True:
+      snapshot.status = "error"
+      snapshot.errorMessage = "Must be logged in to create snapshot."
+      snapshot.put()
+    else:
+      try:
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+        service = discovery.build("tasks", "v1", http)
+
+        tasklist = model.TaskList(parent=snapshot)
+        tasklist.title = self.request.get("name")
+        tasklist.put()
+
+        if self.request.get("format") == "ics":
+          parser = icalparse.Parser(tasklist)
+          tasks_list = parser.ParseAndStore(self.request.get("file"))
+        elif self.request.get("format") == "csv":
+          parser = csvparse.Parser(tasklist)
+          tasks_list = parser.ParseAndStore(self.request.get("file"))
+        else:
+          tasks_list = []
+
+        tasklists = service.tasklists()
+        uploader = apiupload.Uploader(tasklists.insert)
+        tasklist_id = uploader.Upload([tasklist])[0]
+
+        tasks = service.tasks()
+        uploader = apiupload.Uploader(tasks.insert, tasklist=tasklist_id,
+                                      previous=apiupload.PREVIOUS_ARGUMENT)
+        uploader.Upload(tasks_list)
+        snapshot.status = "completed"
+        snapshot.put()
+      except Exception, e:
+        snapshot.status = "error"
+        snapshot.errorMessage = "Snapshot creation process failed unexpectedly."
+        logging.error(e)
+        snapshot.put()
+
+
+def main():
+  application = webapp.WSGIApplication(
+      [
+          ("/worker/snapshot", SnapshotWorker),
+          ("/worker/import", ImportWorker)
+      ])
+  util.run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
